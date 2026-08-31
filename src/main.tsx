95e75761ada4ba3ec7bffd5681d5bbba853a7f9f
dellmastercode2025 <londonenglanduk2015@gmail.com>
2026-08-31 22:52:16 +0500

    Restore browser routing after standalone export

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppStateProvider } from './lib/app-state';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppStateProvider>
         <App />
       </AppStateProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
